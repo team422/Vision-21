943761dfff6c969993b3c7e2e939eb55d7ee7b46
k4rstm4n <quinnkast@gmail.com>
2020-09-18 15:20:42 -0400

    file structure, NT constructors gone, correction

---
+++ CellDetection/java-multiCameraServer/src/main/java/Main.java
@@ -0,0 +1,416 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import edu.wpi.cscore.MjpegServer;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.cscore.VideoSource;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.networktables.EntryListenerFlags;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+import edu.wpi.first.networktables.*;
+
+import edu.wpi.first.vision.VisionPipeline;
+import edu.wpi.first.vision.VisionThread;
+
+import org.opencv.core.Mat;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.HashMap;
+
+import org.opencv.core.*;
+import org.opencv.core.Core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.*;
+import org.opencv.objdetect.*;
+
+
+/*
+   JSON format:
+   {
+       "team": <team number>,
+       "ntmode": <"client" or "server", "client" if unspecified>
+       "cameras": [
+           {
+               "name": <camera name>
+               "path": <path, e.g. "/dev/video0">
+               "pixel format": <"MJPEG", "YUYV", etc>   // optional
+               "width": <video mode width>              // optional
+               "height": <video mode height>            // optional
+               "fps": <video mode fps>                  // optional
+               "brightness": <percentage brightness>    // optional
+               "white balance": <"auto", "hold", value> // optional
+               "exposure": <"auto", "hold", value>      // optional
+               "properties": [                          // optional
+                   {
+                       "name": <property name>
+                       "value": <property value>
+                   }
+               ],
+               "stream": {                              // optional
+                   "properties": [
+                       {
+                           "name": <stream property name>
+                           "value": <stream property value>
+                       }
+                   ]
+               }
+           }
+       ]
+       "switched cameras": [
+           {
+               "name": <virtual camera name>
+               "key": <network table key used for selection>
+               // if NT value is a string, it's treated as a name
+               // if NT value is a double, it's treated as an integer index
+           }
+       ]
+   }
+ */
+
+public final class Main {
+  private static String configFile = "/boot/frc.json";
+
+  @SuppressWarnings("MemberName")
+  public static class CameraConfig {
+    public String name;
+    public String path;
+    public JsonObject config;
+    public JsonElement streamConfig;
+  }
+
+  @SuppressWarnings("MemberName")
+  public static class SwitchedCameraConfig {
+    public String name;
+    public String key;
+  };
+
+  public static int team;
+  public static boolean server;
+  public static List<CameraConfig> cameraConfigs = new ArrayList<>();
+  public static List<SwitchedCameraConfig> switchedCameraConfigs = new ArrayList<>();
+  public static List<VideoSource> cameras = new ArrayList<>();
+  
+
+
+  private Main() {
+  }
+
+  /**
+   * Report parse error.
+   */
+  public static void parseError(String str) {
+    System.err.println("config error in '" + configFile + "': " + str);
+  }
+
+  /**
+   * Read single camera configuration.
+   */
+  public static boolean readCameraConfig(JsonObject config) {
+    CameraConfig cam = new CameraConfig();
+
+    // name
+    JsonElement nameElement = config.get("name");
+    if (nameElement == null) {
+      parseError("could not read camera name");
+      return false;
+    }
+    cam.name = nameElement.getAsString();
+
+    // path
+    JsonElement pathElement = config.get("path");
+    if (pathElement == null) {
+      parseError("camera '" + cam.name + "': could not read path");
+      return false;
+    }
+    cam.path = pathElement.getAsString();
+
+    // stream properties
+    cam.streamConfig = config.get("stream");
+
+    cam.config = config;
+
+    cameraConfigs.add(cam);
+    return true;
+  }
+
+  /**
+   * Read single switched camera configuration.
+   */
+  public static boolean readSwitchedCameraConfig(JsonObject config) {
+    SwitchedCameraConfig cam = new SwitchedCameraConfig();
+
+    // name
+    JsonElement nameElement = config.get("name");
+    if (nameElement == null) {
+      parseError("could not read switched camera name");
+      return false;
+    }
+    cam.name = nameElement.getAsString();
+
+    // path
+    JsonElement keyElement = config.get("key");
+    if (keyElement == null) {
+      parseError("switched camera '" + cam.name + "': could not read key");
+      return false;
+    }
+    cam.key = keyElement.getAsString();
+
+    switchedCameraConfigs.add(cam);
+    return true;
+  }
+
+  /**
+   * Read configuration file.
+   */
+  @SuppressWarnings("PMD.CyclomaticComplexity")
+  public static boolean readConfig() {
+    // parse file
+    JsonElement top;
+    try {
+      top = new JsonParser().parse(Files.newBufferedReader(Paths.get(configFile)));
+    } catch (IOException ex) {
+      System.err.println("could not open '" + configFile + "': " + ex);
+      return false;
+    }
+
+    // top level must be an object
+    if (!top.isJsonObject()) {
+      parseError("must be JSON object");
+      return false;
+    }
+    JsonObject obj = top.getAsJsonObject();
+
+    // team number
+    JsonElement teamElement = obj.get("team");
+    if (teamElement == null) {
+      parseError("could not read team number");
+      return false;
+    }
+    team = teamElement.getAsInt();
+
+    // ntmode (optional)
+    if (obj.has("ntmode")) {
+      String str = obj.get("ntmode").getAsString();
+      if ("client".equalsIgnoreCase(str)) {
+        server = false;
+      } else if ("server".equalsIgnoreCase(str)) {
+        server = true;
+      } else {
+        parseError("could not understand ntmode value '" + str + "'");
+      }
+    }
+
+    // cameras
+    JsonElement camerasElement = obj.get("cameras");
+    if (camerasElement == null) {
+      parseError("could not read cameras");
+      return false;
+    }
+    JsonArray cameras = camerasElement.getAsJsonArray();
+    for (JsonElement camera : cameras) {
+      if (!readCameraConfig(camera.getAsJsonObject())) {
+        return false;
+      }
+    }
+
+    if (obj.has("switched cameras")) {
+      JsonArray switchedCameras = obj.get("switched cameras").getAsJsonArray();
+      for (JsonElement camera : switchedCameras) {
+        if (!readSwitchedCameraConfig(camera.getAsJsonObject())) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  /**
+   * Start running the camera.
+   */
+  //returns camera feed as VideoSource and streams feed to an MjpegServer
+  public static VideoSource startCamera(CameraConfig config) {
+    System.out.println("Starting camera '" + config.name + "' on " + config.path);
+    CameraServer inst = CameraServer.getInstance();
+    UsbCamera camera = new UsbCamera(config.name, config.path);
+    MjpegServer server = inst.startAutomaticCapture(camera);
+
+    Gson gson = new GsonBuilder().create();
+
+    camera.setConfigJson(gson.toJson(config.config));
+    camera.setConnectionStrategy(VideoSource.ConnectionStrategy.kKeepOpen);
+
+    if (config.streamConfig != null) {
+      server.setConfigJson(gson.toJson(config.streamConfig));
+    }
+
+    return camera;
+  }
+
+
+
+
+
+  /**
+   * Start running the switched camera.
+   */
+  public static MjpegServer startSwitchedCamera(SwitchedCameraConfig config) {
+    System.out.println("Starting switched camera '" + config.name + "' on " + config.key);
+    MjpegServer server = CameraServer.getInstance().addSwitchedCamera(config.name);
+
+    NetworkTableInstance.getDefault()
+        .getEntry(config.key)
+        .addListener(event -> {
+              if (event.value.isDouble()) {
+                int i = (int) event.value.getDouble();
+                if (i >= 0 && i < cameras.size()) {
+                  server.setSource(cameras.get(i));
+                }
+              } else if (event.value.isString()) {
+                String str = event.value.getString();
+                for (int i = 0; i < cameraConfigs.size(); i++) {
+                  if (str.equals(cameraConfigs.get(i).name)) {
+                    server.setSource(cameras.get(i));
+                    break;
+                  }
+                }
+              }
+            },
+            EntryListenerFlags.kImmediate | EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
+
+    return server;
+  }
+
+
+
+  /**
+   * Main.
+   */
+  public static void main(String... args) {
+    if (args.length > 0) {
+      configFile = args[0];
+    }
+
+    // read configuration
+    if (!readConfig()) {
+      return;
+    }
+ 
+    // start NetworkTables
+    NetworkTableInstance ntinst = NetworkTableInstance.getDefault();
+
+    if (server) {
+      System.out.println("Setting up NetworkTables server");
+      ntinst.startServer();
+    } else {
+      System.out.println("Setting up NetworkTables client for team " + team);
+      ntinst.startClientTeam(team);
+    }
+
+    //Create/call the table visionTable and assign its entries to NetworkTableEntry variables
+    NetworkTable table = ntinst.getTable("visionTable");
+    NetworkTableEntry testEntry = table.getEntry("test");
+    NetworkTableEntry correctionEntry = table.getEntry("correction");
+
+
+    // start cameras
+    for (CameraConfig config : cameraConfigs) {
+      cameras.add(startCamera(config));
+    }
+
+    // start switched cameras
+    for (SwitchedCameraConfig config : switchedCameraConfigs) {
+      startSwitchedCamera(config);
+    }
+    // start image processing on camera 0 if present
+    if (cameras.size() >= 1) {
+      
+    //Add a new camera stream for displaying the contour on
+    CameraServer inst = CameraServer.getInstance();
+    CvSource drawnVideo = inst.putVideo("visionStream", 160, 120);
+    //These prints were used to get the frame size parameters used in putVideo() so that the new video would be the same size as the camera stream going into the pipeline:
+    // System.out.println(cameras.get(0).getProperty("cols") + "columns in the VideoMode");
+    // System.out.println(cameras.get(0).getProperty("rows") + "rows in the VideoMode");
+      
+      VisionThread visionThread = new VisionThread(cameras.get(0),
+      new CellPipeline(), pipeline -> {
+        // do something with pipeline results
+  
+	      System.out.println("Found " + CellPipeline.findContoursOutput.size() + " contours.");
+        
+        if(CellPipeline.findContoursOutput.size()>0){
+      
+          //Identify the largest contour by comparing each contour to the largest so far, with "largest so far" starting at -1
+          double maxSize = -1;
+          int maxSizeIndex = -1;
+            for(int i = 0; i < CellPipeline.findContoursOutput.size(); i++ ) {
+              if (Imgproc.contourArea(CellPipeline.findContoursOutput.get(i))> maxSize) {
+                maxSize = Imgproc.contourArea(CellPipeline.findContoursOutput.get(i));
+		            maxSizeIndex = i;	  
+	            }
+            }
+
+          //Identify the center X coordinate of a rectangle drawn around the largest contour
+          Rect boundRect = Imgproc.boundingRect(CellPipeline.findContoursOutput.get(maxSizeIndex));
+          double centerX = boundRect.x + (boundRect.width / 2);
+
+          //Find a correction value between -1 and 1 based on the center of the rectangle and the center of the frame
+          double correction = (centerX - (CellPipeline.drawnFrame.width()/2)) * (2/CellPipeline.drawnFrame.width());
+
+          //Set the  networktables entries
+          testEntry.forceSetDouble(422);  
+          correctionEntry.forceSetDouble(correction);
+
+          /*
+          Use OpenCV's drawContours method with parameters:
+          An image in the Mat format to be drawn on: CellPipeline.drawnFrame, the copy of the pipeline input
+          The set of contours where the contour to draw is located: CellPipeline.findContoursOutput
+          The index of the specific contour from the set to draw: maxSizeIndex, the index of the largest contour
+          A scalar with a BGR color to draw in: (255,255,0) or bluish green
+          A thickness to draw in: 2
+          A line format: 8, which is recommended, so go with that
+          */
+          Imgproc.drawContours(CellPipeline.drawnFrame, CellPipeline.findContoursOutput, maxSizeIndex, new Scalar(255,255,0), 2, 8);
+
+          //Now that contours have been drawn on drawnFrame, add it as a frame to the video stream drawnVideo
+          drawnVideo.putFrame(CellPipeline.drawnFrame);
+        }
+      });
+      visionThread.start();
+    }
+
+    // loop forever
+    for (;;) {
+      try {
+        Thread.sleep(10000);
+      } catch (InterruptedException ex) {
+        return;
+      }
+    }
+  }
+}
